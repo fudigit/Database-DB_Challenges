9c526be2d2bbfff990e076a000bbdb2cedbc1d1c
fudigit <30851222+fudigit@users.noreply.github.com>
2019-11-27 23:46:13 -0600

    Update 176.Second_Highest_Salary.sql

--- 176.Second_Highest_Salary.sql
+++ 176.Second_Highest_Salary.sql
@@ -1,1 +1,22 @@
+/* Write your T-SQL query statement below */
 
+WITH Result AS
+    (
+    SELECT
+        DISTINCT(Salary) AS "SecondHighestSalary"
+    FROM Employee
+    ORDER BY Salary DESC         --Order by happens after SELECT!
+        OFFSET 1 ROWS
+        FETCH NEXT 1 ROWS ONLY
+    )
+
+SELECT
+    MAX(SecondHighestSalary) AS "SecondHighestSalary"
+FROM Result
+
+ -- Order by happens after SELECT! In T-SQL, 'ORDER BY col ASC' implies nulls first!
+ -- MAX ignores any null values, returns NULL when there is no row to select
+ -- DISTINCT get rid of the dups
+ -- Usage of OFFSET(rows to skip) and FETCH(numer of rows to return after OFFSET)
+ -- Usage of WITH common_table_expression, specifies a temp result set
+ -- Aggregation function and value cannot be selected at the same time
