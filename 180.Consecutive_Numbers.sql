830482095f82d00ebddeba8fce9603e2fcd119e0
fudigit <30851222+fudigit@users.noreply.github.com>
2019-12-01 22:05:30 -0600

    Update 180.Consecutive_Numbers.sql

--- 180.Consecutive_Numbers.sql
+++ 180.Consecutive_Numbers.sql
@@ -1,1 +1,11 @@
-
+-- v1, join on Ids that are distanced by 1, and check if Num are the same. Do it 2 times, works for 3 consecutive nums
+SELECT
+    DISTINCT(L1.Num) AS 'ConsecutiveNums'
+FROM
+    Logs AS L1
+LEFT JOIN Logs AS L2
+    ON L1.Id + 1 = L2.Id
+LEFT JOIN Logs AS L3
+    ON L2.Id + 1 = L3.ID
+WHERE
+    L1.Num = L2.Num AND L2.Num = L3.Num
